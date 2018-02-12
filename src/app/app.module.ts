34e6ec7b730aba02dd80da93a0899b47d59c2b9f
Christophe Boutter <boutta@gmail.com>
2018-02-12 12:28:54 +0100

    Add Datepicker for Turnier properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -26,7 +28,12 @@ import {DashboardComponent} from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [TurnierService, MessageService],
   bootstrap: [AppComponent]
