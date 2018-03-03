056ef49c179157a4de0e10a67b9dba8d11c32cca
Christophe Boutter <boutta@gmail.com>
2018-03-03 20:01:36 +0100

    Insert admin flag to hide all admin pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {TurnierDetailComponent} from './turnier-detail/turnier-detail.component'
 import {RanglisteComponent} from "./rangliste/rangliste.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: '/rangliste', pathMatch: 'full'},
   {path: 'turniere', component: TurnierComponent},
   {path: 'turnier/:id', component: TurnierDetailComponent},
   {path: 'dashboard', component: DashboardComponent},
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SBV Rangliste';
+  admin = true;
 }
