db1519e33a75396e78a4ac091fa6ed7d79cccf4b
Christophe Boutter <boutta@gmail.com>
2018-03-03 13:26:54 +0100

    Add route to Rangliste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {TurnierComponent} from './turnier/turnier.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {TurnierDetailComponent} from './turnier-detail/turnier-detail.component';
+import {RanglisteComponent} from "./rangliste/rangliste.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'turniere', component: TurnierComponent},
   {path: 'turnier/:id', component: TurnierDetailComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'rangliste', component: RanglisteComponent}
 ];
 
 @NgModule({
