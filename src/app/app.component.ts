9c449e02473b85c621a2cfecc7230ecd14217cc2
Christophe Boutter <boutta@gmail.com>
2018-03-07 18:29:14 +0100

    Admin-flag working from URL query params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SBV Rangliste';
-  admin = true;
+  admin = false;
+
+  constructor(private route: ActivatedRoute) {
+    console.log(route);
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.admin = params['admin'];
+    });
+  }
 }
